0f95f41c04e6294139d59699e42d91555056d59c
SPENCER DURRANT <spencerdurrant@gmail.com>
2021-04-28 22:06:39 -0600

    first start

--- src/autopipe.py
+++ src/autopipe.py
@@ -4,11 +4,12 @@ import numpy as np
 import pandas as pd
 import os, sys
 from pathlib import Path
-import hdlparse.verilog_parser as vlog
+import regex as re
 
 def load_path(rel_path):
     """
     Gets the exact file path relative to this script executable location
+    Used with a scripting environment (e.g. google colab or jupyter notebooks)
 
     Parameters
     ----------
@@ -24,11 +25,15 @@ def load_path(rel_path):
     p = os.path.join(current_folder, rel_path)
     return p
 
+def load_file(f_name):
+    p = Path(__file__).with_name(f_name)
+    return p
+
 
 args = sys.argv
 # set defaults
-vfile = 'c880.v'
-stage = 4
+vfile = 'adder2-comb.v'
+stage = 3
 direction = 'f'
 
 for a in range(len(args)):
@@ -41,7 +46,33 @@ for a in range(len(args)):
     elif args[a] == '-forward': # not necessary
         direction = 'f'
     
-path = load_path(vfile)
+
+
+path = str(load_file(vfile))
+
+with open(vfile) as f:
+    content = f.read().splitlines()
+
+m_name = ""
+inputs = []
+curr = ""
+for line in content:
+    pattern = "[" + ";()," + "]"
+    line = re.sub(pattern, "", line)
+
+    if line.startswith("module"):
+        m_name = line.split()[1]
+        continue
+    elif line.startswith("input"):
+        sub_str = line.split()
+        inputs.append(sub_str[1:])
+        # search next lines
+
+    elif line.startswith("output"):
+        sub_str = line.split()
+        inputs.append(sub_str[1:])
+        # search next lines
+    
 
 # verilog File structure
 #
@@ -53,3 +84,5 @@ path = load_path(vfile)
 # endmodule
 # note only variable name \1 has the slash, and it always carries it
 
+# generate input/forward .v file with all inputs registered
+# generate output/backward .v file with all outputs registered
