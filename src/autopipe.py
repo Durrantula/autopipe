4ebd22f79ef5434a2c1065e2cdab32a233a537b2
SPENCER DURRANT <spencerdurrant@gmail.com>
2021-04-28 23:20:00 -0600

    finished parsing, now generating input

--- src/autopipe.py
+++ src/autopipe.py
@@ -30,6 +30,95 @@ def load_file(f_name):
     return p
 
 
+def after_key(line, lst):
+    sub_str = line.split()
+    lst.append(sub_str[1:])
+
+def parse_verlg(path):
+    with open(path) as f:
+        content = f.read().splitlines()
+
+    key_names = ("module", "input", "output", "wire", "assign", "endmodule")
+    m_name = ""
+    inputs = []
+    outputs = []
+    wires = []
+    assigns = []
+    curr = ""
+    for i, line in enumerate(content):
+        pattern = "[" + ";()," + "]"
+        line = re.sub(pattern, "", line)
+
+        if line.startswith("module"):
+            m_name = line.split()[1]
+            continue
+
+        elif line.startswith("input"):
+            after_key(line, inputs)
+            curr = "input"
+            
+        elif line.startswith("output"):
+            after_key(line, outputs)
+            curr = "output"
+
+        elif line.startswith("wire"):
+            after_key(line, wires)
+            curr = "wire"
+
+        elif line.startswith("assign"):
+            assigns.append(line)    # maybe split
+            curr = ""               # assign new_n8_ = a0 & b0
+
+        elif line.startswith("endmodule"):
+            curr = ""
+            break
+        else:
+            if curr == "input":
+                if i != len(content) and content[i+1].startswith(key_names):
+                    curr = ""
+                inputs.append(line.split())
+
+            elif curr == "output":
+                if i != len(content) and content[i+1].startswith(key_names):
+                    curr = ""
+                outputs.append(line.split())
+
+            elif curr == "wire":
+                if i != len(content) and content[i+1].startswith(key_names):
+                    curr = ""
+                wires.append(line.split())
+    
+    return inputs, outputs, wires, assigns, m_name
+
+
+def generate_forward(inputs, outputs, wires, assigns, m_name):
+    registers = ["\\" + m_name + "|s_" + inp for inp in inputs]
+
+    # create always block string
+    always_str = "always @ (posedge clock) begin\n"
+    
+    reg_o_wires = []
+    for i in range(len(inputs)):
+        reg_o_wires.append("new_w" + i)
+
+    # make new assigns for the new wires and inputs
+
+    for i, reg in enumerate(registers):
+        always_str = always_str + reg + "<=" + reg_o_wires[i] + ";\n"
+
+    # Everywhere we see an input[i] in an assign statement, we replace
+    # the input[i] with reg_o_wires[i]??
+
+
+
+    always_str = always_str + "end\nendmodule\n"
+
+    big_string_lol = "BEEF"
+    return big_string_lol
+
+
+
+
 args = sys.argv
 # set defaults
 vfile = 'adder2-comb.v'
@@ -50,29 +139,10 @@ for a in range(len(args)):
 
 path = str(load_file(vfile))
 
-with open(vfile) as f:
-    content = f.read().splitlines()
+inputs, outputs, wires, assigns, m_name = parse_verlg(path)
 
-m_name = ""
-inputs = []
-curr = ""
-for line in content:
-    pattern = "[" + ";()," + "]"
-    line = re.sub(pattern, "", line)
+forward_v = generate_forward(inputs, outputs, wires, assigns, m_name)
 
-    if line.startswith("module"):
-        m_name = line.split()[1]
-        continue
-    elif line.startswith("input"):
-        sub_str = line.split()
-        inputs.append(sub_str[1:])
-        # search next lines
-
-    elif line.startswith("output"):
-        sub_str = line.split()
-        inputs.append(sub_str[1:])
-        # search next lines
-    
 
 # verilog File structure
 #
